78f6d12b42ee8e6c6eccaf475ac808d09641b2ec
whoiscc <correctizer@gmail.com>
2019-11-02 14:06:53 +0800

    Rewrite with scoped ref and mut API
    Signed-off-by: whoiscc <correctizer@gmail.com>

--- borrow_checker.js
+++ borrow_checker.js
@@ -9,133 +9,90 @@ class ObjectProxy {
     }
 
     with_ref(handler) {
-        this.inc_ref();
-        this._object = handler(this._object);
-        this.dec_ref();
-    }
-
-    with_mut(handler) {
-        this.create_mut();
-        this._object = handler(this._object);
-        this.destroy_mut();
-    }
-
-    with_move(handler) {
-        this.set_moved();
-        handler(this._object);
-    }
-
-    as_ref() {
-        return new ObjectRef(this);
-    }
-
-    as_mut() {
-        return new ObjectMut(this);
-    }
-
-    move() {
-        return new ObjectMove(this);
-    }
-
-    inc_ref() {
         if (this._mut || this._moved) {
             throw new Error('Cannot get ref');
         }
         this._ref += 1;
-    }
-
-    dec_ref() {
-        if (this._ref == 0) {
-            throw new Error('No ref left');
+        try {
+            handler(new ObjectRef(this));
+        } finally {
+            if (this._ref == 0) {
+                throw new Error('No ref left');
+            }
+            this._ref -= 1;    
         }
-        this._ref -= 1;
     }
 
-    create_mut() {
+    with_mut(handler) {
         if (this._ref != 0 || this._mut || this._moved) {
             throw new Error('Cannot get mut');
         }
         this._mut = true;
-    }
-
-    destroy_mut() {
-        if (!this._mut) {
-            throw new Error('No mut created');
+        try {
+            handler(new ObjectMut(this));
+        } finally {
+            if (!this._mut) {
+                throw new Error('No mut created');
+            }
+            this._mut = false;    
         }
-        this._mut = false;
     }
 
-    set_moved() {
+    move() {
         if (this._ref != 0 || this._mut || this._moved) {
             throw new Error('Cannot move');
         }
         this._moved = true;
+        return new ObjectProxy(this._object);
+    }
+
+    _capture_ref(handler) {
+        handler(this._object);
+    }
+
+    _capture_mut(handler) {
+        this._object = handler(this._object);
     }
 }
 
 class ObjectRef {
     constructor(proxy) {
-        this.proxy = proxy;
+        this._proxy = proxy;
     }
 
-    enter() {
-        this.proxy.inc_ref();
-    }
-
-    exit() {
-        this.proxy.dec_ref();
+    capture(handler) {
+        this._proxy._capture_ref(handler);
     }
 }
 
 class ObjectMut {
     constructor(proxy) {
-        this.proxy = proxy;
+        this._proxy = proxy;
     }
 
-    enter() {
-        this.proxy.create_mut();
-    }
-
-    exit() {
-        this.proxy.destroy_mut();
-    }
-}
-
-class ObjectMove {
-    constructor(proxy) {
-        this.proxy = proxy;
-    }
-
-    enter() {
-        this.proxy.set_moved();
-    }
-
-    exit() {
-        //
-    }
-}
-
-function wrap(func) {
-    return function () {
-        for (arg of arguments) {
-            arg.enter();
-        }
-        const result = func(...arguments);
-        for (arg of arguments) {
-            arg.exit();
-        }
-        return result;
+    capture(handler) {
+        this._proxy._capture_mut(handler);
     }
 }
 
 const n = new ObjectProxy(42);
-wrap((a, b) => {
-    console.log(a, b);
-})(n.as_ref(), n.as_ref());
+n.with_ref(ref_n => {
+    ref_n.capture(num => {
+        console.log('The number is ' + num.toString() + '.');
+    });
+});
+
 try {
-    wrap((a, b) => {
-        console.log(a, b);
-    })(n.as_ref(), n.as_mut());
+    n.with_mut(mut_n => {
+        n.with_ref(ref_n => {});
+    });
+} catch (err) {
+    console.log(err);
+}
+
+const m = n.move();
+try {
+    n.with_ref(ref_n => {});
 } catch (err) {
     console.log(err);
 }
